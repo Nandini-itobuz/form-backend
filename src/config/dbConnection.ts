59241ccdc2fc2c4b13e0f584a64ca3821ccf3c32
Nandini Kedia <nandinikedia@Nandinis-MacBook-Pro.local>
2024-07-15 13:31:27 +0530

    fix

--- src/config/dbConnection.ts
+++ src/config/dbConnection.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const dbConnection = () => {
-	mongoose.connect(process.env.MONGOURL!).
+	mongoose.connect(process.env.MONGO_URL!).
 		then(() => console.log('Connected to MongoDB'))
 		.catch(err => console.log('Failed to connect to MongoDB:', err));
 }
