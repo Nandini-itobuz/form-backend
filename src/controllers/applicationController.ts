96632ddc855346ec5b6e27e1f55838a8a33fddaf
Nandini Kedia <nandinikedia@Nandinis-MacBook-Pro.local>
2024-07-15 13:28:55 +0530

    fix

--- src/controllers/applicationController.ts
+++ src/controllers/applicationController.ts
@@ -1,29 +1,28 @@
 import { Request, Response } from "express";
 import { applicationModel } from "../schemas/applicationSchema";
 import mongoose from "mongoose";
-import { start } from "repl";
 
 
-const responseData = (res: Response, success: boolean, status: Number, data: any) => {
-    res.status(200).json({ data, status, success });
+const responseData = (res: Response, statusCode: number) => {
+    res.status(200).json({ data: null, status: statusCode, success: false});
 }
 
 export async function writeDetails(req: Request, res: Response): Promise<void> {
     try {
         const newDetails = await applicationModel.create(req.body);
-        responseData(res, true, 202, newDetails)
+        res.status(200).json({ data :newDetails, status : 200, success : true });
     } catch (err) {
         console.log(err)
-        responseData(res, false, 400, null)
+        responseData(res, 400)
     }
 }
 
 export async function findDetails(req: Request, res: Response): Promise<void> {
     try {
         const data = await applicationModel.findOne({ _id: new mongoose.Types.ObjectId(req.params.id) });
-        responseData(res, true, 202, data)
+        res.status(200).json({ data , status : 200, success : true });
     } catch (err) {
-        responseData(res, false, 400, null)
+        responseData(res, 400)
     }
 }
 
@@ -37,19 +36,19 @@ export async function getAllDetails(req: Request, res: Response): Promise<void> 
         const paginatedData = userDetails.slice(startIndex, endIndex);
         const totalPages = Math.ceil(userDetails.length / pageSize);
         const applications = {applicationData : paginatedData ,totalPages:totalPages }
-        responseData(res, true, 202, applications)
+        res.status(200).json({ data :applications, status : 200, success : true });
 
     } catch (err) {
-        responseData(res, false, 400, null)
+        responseData(res, 400)
     }
 }
 
 export async function deleteDetail(req: Request, res: Response): Promise<void> {
     try {
         const userDetails = await applicationModel.findByIdAndDelete({ _id: req.params.id });
-        responseData(res, true, 202, userDetails)
+        res.status(200).json({ data :userDetails, status : 200, success : true });
     } catch (err) {
-        responseData(res, false, 400, null)
+        responseData(res, 400)
     }
 }
 
@@ -58,26 +57,26 @@ export async function updateDetail(req: Request, res: Response): Promise<void> {
         const userDetails = await applicationModel.findByIdAndUpdate({ _id: req.params.id }, req.body, {
             returnOriginal: false
         });
-        responseData(res, true, 202, userDetails)
+        res.status(200).json({ data :userDetails, status : 200, success : true });
     } catch (err) {
-        responseData(res, false, 400, null)
+        responseData(res, 400)
     }
 }
 
 export async function getApplicationsByPositions(req: Request, res: Response): Promise<void> {
     try {
         const applications = await applicationModel.find({ position: req.params.position })
-        responseData(res, true, 202, applications)
+        res.status(200).json({ data :applications, status : 200, success : true });
     } catch (err) {
-        responseData(res, false, 400, null)
+        responseData(res, 400)
     }
 }
 
 export async function deleteApplications(req: Request, res: Response): Promise<void> {
     try {
         const applications = await applicationModel.deleteMany({});
-        responseData(res, true, 202, applications)
+        res.status(200).json({ data :applications, status : 200, success : true });
     } catch (err) {
-        responseData(res, false, 400, null)
+        responseData(res,400)
     }
 }
